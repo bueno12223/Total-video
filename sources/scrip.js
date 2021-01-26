555821ffbc9e61a5d4539198e92dc0b63d2bd428
Jesus <berriojesus122@gmail.com>
2021-01-26 10:35:01 -0500

    logica para play y pause

--- sources/scrip.js
+++
@@ -1,8 +0,0 @@
-const video = document.querySelector("video")
-        const button = document.querySelector("button")
-        function mediaPlayer(conf) {
-            this.media = config.el
-        }
-        mediaPlayer.prototype.play = function (){ this.media.play()}
-        const player = new mediaPlayer({ el: video })
-        button.onclick = () => video.play()
