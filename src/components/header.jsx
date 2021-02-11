f612a16243c7a4e2e4885292df327f4d86426822
Jesus <berriojesus122@gmail.com>
2021-02-11 17:22:34 -0500

    cree el primer componente y su configuracion para ser renderizado

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "./style/style.css"
+
 const header = () => {
     return(
         <header className="header">
@@ -19,3 +21,4 @@ const header = () => {
     )
 
 }
+export default header
---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/header';
+import "../assets/App.sccs"
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
