001de374453eb01e579ada3335a1bf9c6f191ce9
Jesus <berriojesus122@gmail.com>
2021-02-11 18:02:22 -0500

    compnente de search añadido

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/header';
+import Search from "../components/Search"
+
 import "../assets/style/App.scss"
+
 
 const App = () => (
   <div className="App">
     <Header />
+    <Search/>
   </div>
 );
 
