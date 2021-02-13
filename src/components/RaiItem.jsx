23b4e5494b27a522b08977bde47a294351ae52b1
Jesus <berriojesus122@gmail.com>
2021-02-13 18:12:26 -0500

    API agregada y funcionando

--- src/components/RaiItem.jsx
+++ src/components/RaiItem.jsx
@@ -4,13 +4,13 @@ import React, {children} from "react"
 import "../assets/style/components/railItem.scss"
 import pause from "../assets/images/pause.png"
 
-const railitem = ({cover, title, year, contentRating, duration}) => (
+const railitem = ({overview, poster_path, runtime}) => 
+(
     <div className="rail__item">
-                <img className="rail_cover" src={cover} alt={title}/>
-                <p className="disapear">{title}</p>
-                <p className="rail__description">{`${year}  ${contentRating}  ${duration}` }</p>
+                <img className="rail_cover" src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt=""/>
+                
                 <span><img src="https://img.icons8.com/flat_round/64/000000/play--v1.png"/></span>
-                <span><img className="rail__pause" src={pause} alt=""/></span>
+                
             </div>
 )
 export default railitem
--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -5,38 +5,48 @@ import Categories from "../components/categories";
 import Rail from "../components/Rail"
 import RailItem from "../components/RaiItem"
 import Footer from "../components/footer"
-import useGetMovies from "../hooks/useGetMovies"
+
 
 import "../assets/style/App.scss"
 import "../assets/style/components/railItem.scss"
 
 class home extends React.Component{ 
+  // 
 
   constructor(props) {
     super(props);
     this.state = {
     loading: true,
     error: null,
-    movies: {
-      // id: null,
-      // name: '',
-      // cover: '',
-      // overview: '',
-      // genres: '',
-    },
+    movies: [],
+    myList: {
+      id: [],
+      movies: {}
+    }
   } }
 
 
   componentDidMount(){
-    this.searchData(400)
+    for(let i = 0; i < 5; i++){
+      const ramdomMovie = this.random(30,550)
+      console.log(ramdomMovie)
+      this.searchData(ramdomMovie)
+
+    }
+    
+    
   }
+  random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
   async searchData(id){
     const key = "0c7a6b113add233831a0d6eec346cd98"
     try{
  
     const data = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`) 
     const res = await data.json()
-    this.setState({loading: false, movies:res})
+    this.state.movies.push(res)
+    this.setState({loading: false})
     
     }catch(error){
       this.setState({loading: false, error: error})
@@ -49,26 +59,26 @@ return (
   <div className="App">
     <Header />
     <Search/>
-    {/* {initialState.mylist.length > 0 && ( */}
+     {this.state.myList > 0 &&( 
       <React.Fragment>
       <Categories  title="Mi lista"></Categories>
       <Rail>
+
         <RailItem/>
         
       </Rail>
-      </React.Fragment>
+      </React.Fragment> 
 
-    )
-    
-
+    ) }
     
 
       <Categories  title="Mas vistas"></Categories>
       <Rail>
-        {/* {useGetMovies.trends.map(item =>
-           <RailItem key={item.id} {...item} />)} */}
-           <RailItem></RailItem>
-           {console.log(this.state)}
+      {this.state.movies.map(item =>
+           <RailItem key={item.id} {...item} />)}
+         
+           
+           
         
        
       </Rail>
@@ -76,7 +86,7 @@ return (
       <Rail>
       {/* {initialState.originals.map(item =>
            <RailItem key={item.id} {...item} />)} */}
-           <RailItem></RailItem>
+           
       
       </Rail>
       <Footer></Footer>
