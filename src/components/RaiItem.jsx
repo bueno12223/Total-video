dc1d35e95be34772d217df3a692f23196261afb0
Jesus <berriojesus122@gmail.com>
2021-02-13 14:11:47 -0500

    Agregué la API y algunos detalles finales de CSS

--- src/components/RaiItem.jsx
+++ src/components/RaiItem.jsx
@@ -4,9 +4,11 @@ import React, {children} from "react"
 import "../assets/style/components/railItem.scss"
 import pause from "../assets/images/pause.png"
 
-const railitem = ({id}) => (
-    <div className="rail__item rail1 " id={id} >
-                <p className="disapear">Una entretenida aventura </p>
+const railitem = ({cover, title, year, contentRating, duration}) => (
+    <div className="rail__item">
+                <img className="rail_cover" src={cover} alt={title}/>
+                <p className="disapear">{title}</p>
+                <p className="rail__description">{`${year}  ${contentRating}  ${duration}` }</p>
                 <span><img src="https://img.icons8.com/flat_round/64/000000/play--v1.png"/></span>
                 <span><img className="rail__pause" src={pause} alt=""/></span>
             </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,50 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '../components/header';
 import Search from "../components/Search";
 import Categories from "../components/categories";
 import Rail from "../components/Rail"
 import RailItem from "../components/RaiItem"
 import Footer from "../components/footer"
+import useInitialState from "../hooks/useInitialState"
+
+const API =  "http://localhost:3000/initialState"
 
 import "../assets/style/App.scss"
 import "../assets/style/components/railItem.scss"
 
 
-const App = () => (
+const App = () => { 
+  const initialState = useInitialState(API)
+
+return (
   <div className="App">
     <Header />
     <Search/>
-
-    <Categories  title="Mi lista"></Categories>
+    {initialState.mylist.length > 0 && (
+      <React.Fragment>
+      <Categories  title="Mi lista"></Categories>
       <Rail>
-        <RailItem id="uno" />
-        <RailItem id="b" />
-        <RailItem id="c" />
-        <RailItem id="d" />
-        <RailItem id="e" />
+        <RailItem/>
+        
       </Rail>
+      </React.Fragment>
+
+    )}
+    
+
+    
 
       <Categories  title="Mas vistas"></Categories>
       <Rail>
-        <RailItem id="d" />
-        <RailItem id="uno" />
-        <RailItem id="e" />
-        <RailItem id="uno" />
-        <RailItem id="b" />
+        {initialState.trends.map(item =>
+           <RailItem key={item.id} {...item} />)}
+        
+       
       </Rail>
 
       <Categories  title="Para tí"></Categories>
       <Rail>
-        <RailItem id="c"/>
-        <RailItem id="uno" />
-        <RailItem id="b" />
-        <RailItem id="d" />
-        <RailItem id="e" />
+      {initialState.originals.map(item =>
+           <RailItem key={item.id} {...item} />)}
+      
       </Rail>
       <Footer></Footer>
 
    
   </div>
 );
+  }
 
 export default App;
