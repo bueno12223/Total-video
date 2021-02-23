b0bd54f16b14cb1bde80248dcacd97ddf9f93ba1
Jesus <berriojesus122@gmail.com>
2021-02-22 19:28:17 -0500

    header modificado

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Switch} from "react-router-dom"
 
 import Home from "./pages/home"
 import Login from "./pages/login"
-
 import Layout from "./components/layout"
 import NotFoud from "./components/notFoud"
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 import "../assets/style/components/header.scss"
 
@@ -6,15 +7,21 @@ const header = () => {
     return(
         <header className="header">
         <div  className="header__containerImage">
-        
+            <Link to ="/">
             <img src="https://img.icons8.com/wired/64/ffffff/music-video.png" className="header__image" />
-            <p>Total Video</p>
+            </Link>
+            <Link to="/"><p className="header__total">Total <b> Video</b></p></Link> 
+            
+            
+            
+        
+            
         </div>
         <div className="header__user">
             <img src="https://img.icons8.com/wired/64/ffffff/change-user-male.png" className="header__userImage"/>
             <ul className="header__userText">
                 <li><a href="">Cuenta</a></li>
-                <li><a href="">Cerrar Sesión</a></li>
+                <Link to="/login"><li><a href="">Cerrar Sesión</a></li></Link>
             </ul>
         </div>
     </header>
